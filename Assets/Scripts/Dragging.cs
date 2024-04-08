e7b844039f7a619dd84b34923af5426c996e8d07
MRbubbelgum <143446220+MRbubbelgum@users.noreply.github.com>
2024-04-08 15:53:46 +0200

    DragControllerUpdate

--- Assets/Scripts/Dragging.cs
+++ Assets/Scripts/Dragging.cs
@@ -4,31 +4,4 @@ using UnityEngine;
 
 public class Dragging : MonoBehaviour
 {
-    private bool isDragActive = false;
-    private Vector2 screenPosition;
-    private Vector3 worldPosition;
-    private Draggable lastDragged;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    private void InitDrag()
-    {
-
-    }
-    private void Drag()
-    {
-
-    }
-    private void Drop()
-    {
-
-    }
 }
