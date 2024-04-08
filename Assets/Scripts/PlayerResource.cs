90455f6e1898476a4120edfca6d12c5f4bba44a4
Jolbs <joel.sundstrom@telia.com>
2024-04-08 14:26:27 +0200

    SceneJoelSund + TMP imports

---
+++ Assets/Scripts/PlayerResource.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+
+public class PlayerResource : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Ska finnas ett startvärde
+
+    [SerializeField] public int Resource = 100;
+
+    public int updateResource;
+    public TMP_Text textObject;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    
+    }
+
+        //Ska finnas en funtkion som uppdaterar resursmängd
+        public int updateResource
+    {
+        //on enemy kill, resource++;
+    }
+}   
+    
+
+//Ska finnas en metod som adderar när en fiende dör
+
+
+
