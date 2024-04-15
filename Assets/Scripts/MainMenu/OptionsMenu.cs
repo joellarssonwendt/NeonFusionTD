1feedc39d80501b5efc1cbf49710a07d5c7ea50f
Ryxeus <furkanbasa@hotmail.com>
2024-04-15 16:57:50 +0200

    Fixed overlapping buttons on start
    Fixed the bug where the Back and Quit buttons overlapped on Start

---
+++ Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject options;
+    private void Start()
+    {
+        options.SetActive(false);
+    }
+}
