bcb7f5744b3d6ffccde8c66106a16f1d6819bb8f
MRbubbelgum <malte.gundhus@gmail.com>
2024-04-16 17:48:55 +0200

    Design Update

---
+++ Assets/Scripts/diamondBits.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class diamondBits : MonoBehaviour
+{
+    private TextMeshPro bitsAmountText;
+    public int bitsAmount;
+    private bool hasStarted = false;
+    void Start()
+    {
+        bitsAmountText = GetComponent<TextMeshPro>();
+        bitsAmount = 100;
+        hasStarted = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasStarted && bitsAmountText != null)
+        {
+            bitsAmountText.text = bitsAmount.ToString();
+        }
+    }
+}
