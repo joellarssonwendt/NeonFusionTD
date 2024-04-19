b812de3a452c7cdd0284ef50f16724d3dbfb20af
Ryxeus <furkanbasa@hotmail.com>
2024-04-19 15:22:08 +0200

    Added How To Play Menu
    can be found on the main menu

---
+++ Assets/Scripts/MainMenu/HowToPlayMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayMenu : MonoBehaviour
+{
+    public GameObject howToPlayMenu;
+    private void Start()
+    {
+        howToPlayMenu.SetActive(false);
+    }
+}
