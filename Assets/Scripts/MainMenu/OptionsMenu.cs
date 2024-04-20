f18386c45765def25c7ab04fb8175da4e7f4fb4d
87ch87 <ch.huusko@gmail.com>
2024-04-20 17:31:15 +0200

    Big AudioManager update and 3 audio sliders in main menu

--- Assets/Scripts/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -1,12 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject options;
+    public Slider musicVolumeSlider;
+    public Slider soundEffectsVolumeSlider;
+    public Slider UISoundEffectsVolumeSlider;
+    public AudioManager audioManager;
+
     private void Start()
     {
         options.SetActive(false);
+
+        musicVolumeSlider.value = audioManager.MusicVolume;
+        soundEffectsVolumeSlider.value = audioManager.SoundEffectsVolume;
+        UISoundEffectsVolumeSlider.value = audioManager.UISoundEffectsVolume;
+    }
+
+    public void OnMusicVolumeChanged(float value)
+    {
+        audioManager.MusicVolume = value;
+        audioManager.UpdateMusicVolume();
+    }
+
+    public void OnSoundVolumeChanged(float value)
+    {
+        audioManager.SoundEffectsVolume = value;
+        audioManager.UpdateSoundEffectsVolume();
+    }
+
+    public void OnUISoundVolumeChanged(float value)
+    {
+        audioManager.UISoundEffectsVolume = value;
+        audioManager.UpdateUISoundEffectsVolume();
     }
 }
