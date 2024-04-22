12159fcb0f97e47445f278ed6de0697b052ea763
MRbubbelgum <malte.gundhus@gmail.com>
2024-04-22 21:49:47 +0200

    123

--- Assets/Scripts/ScaleUI.cs
+++ Assets/Scripts/ScaleUI.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ScaleUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private GameObject diamondIcon;
+    [SerializeField] private GameObject menuIcon;
+    [SerializeField] private GameObject shop;
+    [SerializeField] private GameObject healthBar;
+    [SerializeField] private GameObject mergeArchive;
+
+    private float moveUpperIconsValue = 80f;
     void Start()
     {
+
         float aspectRatio = (float)Screen.width / Screen.height;
         float aspectRatioWidthLimit = 9f / 16f;
 
-        if(aspectRatio > aspectRatioWidthLimit)
+        if (aspectRatio > aspectRatioWidthLimit)
         {
             Debug.Log("Nu händer de grejer för skärmen är bred.");
+            diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, diamondIcon.transform.position.y + moveUpperIconsValue, diamondIcon.transform.position.z);
+            menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, menuIcon.transform.position.y + moveUpperIconsValue, menuIcon.transform.position.z);
         }
         else
         {
@@ -23,6 +33,6 @@ public class ScaleUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
