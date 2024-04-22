d67ab08192a7dfbe156cb4de8799092d54be7792
MRbubbelgum <malte.gundhus@gmail.com>
2024-04-22 22:16:37 +0200

    update

--- Assets/Scripts/ScaleUI.cs
+++ Assets/Scripts/ScaleUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScaleUI : MonoBehaviour
 {
@@ -11,7 +12,9 @@ public class ScaleUI : MonoBehaviour
     [SerializeField] private GameObject healthBar;
     [SerializeField] private GameObject mergeArchive;
 
-    private float moveUpperIconsValue = 80f;
+    private float moveUpperIconsValue = 120f;
+    private float shopScaleWideScreen = 0.8f;
+    private float shopHeightWideScreen = 180f;
     void Start()
     {
 
@@ -23,6 +26,8 @@ public class ScaleUI : MonoBehaviour
             Debug.Log("Nu händer de grejer för skärmen är bred.");
             diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, diamondIcon.transform.position.y + moveUpperIconsValue, diamondIcon.transform.position.z);
             menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, menuIcon.transform.position.y + moveUpperIconsValue, menuIcon.transform.position.z);
+            shop.GetComponent<RectTransform>().sizeDelta = new Vector2(shop.GetComponent<RectTransform>().sizeDelta.x, shopHeightWideScreen);
+            //shop.transform.localScale = new Vector2(shopScaleWideScreen, shopScaleWideScreen);
         }
         else
         {
