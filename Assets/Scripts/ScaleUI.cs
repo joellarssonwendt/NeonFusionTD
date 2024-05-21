fa5b9a301512efcff82fe2ed525aa77833d5c6e0
MRbubbelgum <malte.gundhus@gmail.com>
2024-05-21 16:32:08 +0200

    UI Scale Update, ska tas bort.
     lösningen jag fixade verkar inte fungera ordentligt. Den går att scalea bra i realtid men av någon anledning så är det jätte stor skillnad mellan olika enheter med den här lösningen vilket gör att det inte fungerar.

--- Assets/Scripts/ScaleUI.cs
+++ Assets/Scripts/ScaleUI.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ScaleUI : MonoBehaviour
 {
@@ -15,31 +12,57 @@ public class ScaleUI : MonoBehaviour
     private Vector2 lastScreenSize;
 
     //fat settings
-    private float cameraPositionFat = -0.4f;
-    private float moveUpperIconsValue = 120f;
-    private float shopScaleFatScreen = 0.8f;
-    private float shopHeightFatScreen = 180f;
-    private float healthBarPositionYFatScreen = 290f;
-    private float healthBarScaleFatScreen = 0.79f;
-    private float mergeFatScreenMoveValue = 100f;
-    private float nextRoundButtonFatMoveValue = 80f;
-
-
-
-    //thin settings
-    private float cameraPositionThinMobile = 0.8f;
-    private float shopHeightThinScreen = 200f;
-    private float healthBarPositionValueThinScreen = 10f;
-
-    //normal settings
-    private float upperIconsMoveNormalValue = 65f;
+    [Header("Fat Screen Values:")]
+    [SerializeField] private float cameraPositionFat;
+    [SerializeField] private float moveUpperIconsFatValue;
+    [SerializeField] private float moveUpperMenuButtonsFatValue;
+    [SerializeField] private float shopScaleFatScreen;
+    [SerializeField] private float shopHeightFatScreen;
+    [SerializeField] private float healthBarPositionYFatScreen;
+    [SerializeField] private float healthBarScaleFatScreen;
+    [SerializeField] private float mergeFatScreenMoveYValue;
+    [SerializeField] private float mergeFatScreenMoveXValue;
+    [SerializeField] private float nextRoundButtonFatMoveValue;
 
     //wide settings
-    private float upperIconsMoveWhenWide = 40f;
-    private float healthBarPositionYWideScreen = 92f;
-    private float healthBarScaleWideScreen = 0.89f;
-    private float shopScaleWideScreen = 0.9f;
-    private float shopHeightWideScreen = 185f;
+    [Header("Wide Screen Values:")]
+    [SerializeField] private float cameraPositionWide;
+    [SerializeField] private float moveUpperMenuButtonsWideValue;
+    [SerializeField] private float upperIconsMoveWhenWide;
+    [SerializeField] private float shopScaleWideScreen;
+    [SerializeField] private float shopHeightWideScreen;
+    [SerializeField] private float healthBarPositionYWideScreen;
+    [SerializeField] private float healthBarScaleWideScreen;
+    [SerializeField] private float mergeWideScreenMoveYValue;
+    [SerializeField] private float mergeWideScreenMovexValue;
+    [SerializeField] private float nextRoundButtonWideMoveValue;
+
+
+    //normal settings
+    [Header("Normal Screen Values:")]
+    [SerializeField] private float cameraPositionNormal;
+    [SerializeField] private float moveUpperMenuButtonsValue;
+    [SerializeField] private float upperIconsMoveNormalValue;
+    [SerializeField] private float shopScaleNormalScreen;
+    [SerializeField] private float shopHeightNormalScreen;
+    [SerializeField] private float healthBarPositionYNormalScreen;
+    [SerializeField] private float healthBarScaleNormalScreen;
+    [SerializeField] private float mergeNormalScreenMoveYValue;
+    [SerializeField] private float mergeNormalScreenMoveXValue;
+    [SerializeField] private float nextRoundButtonNormalMoveValue;
+
+    [Header("Thin Screen Values:")]
+    //thin settings
+    [SerializeField] private float cameraPositionThinMobile;
+    [SerializeField] private float moveUpperMenuButtonsValueThin;
+    [SerializeField] private float upperIconsMoveNormalValueThin;
+    [SerializeField] private float shopScaleThinScreen;
+    [SerializeField] private float shopHeightThinScreen;
+    [SerializeField] private float healthBarPositionValueThinScreen;
+    [SerializeField] private float healthBarScaleThinScreen;
+    [SerializeField] private float mergeThinScreenMoveYValue;
+    [SerializeField] private float mergeThinScreenMoveXValue;
+    [SerializeField] private float nextRoundButtonThinMoveValue;
 
     //resolutions
     private float aspectRatioWide = 0.7f; // lite större än 2:3
@@ -55,81 +78,122 @@ public class ScaleUI : MonoBehaviour
         if (aspectRatio > aspectRatioWide)
         {
             Debug.Log("Nu händer de grejer för skärmen är Ipad bred.");
-            MoveCameraWhenFat();
-            MoveIconsWhenFat();
-            ScaleMoveShopWhenFat();
-            ScaleMoveHealthbarWhenFat();
-            MoveNextRoundButtonWhenFat();
+            ChangeValuesForFatResolution();
         }
         else if (aspectRatio < aspectRatioThinMobile)
         {
             Debug.Log("Nu händer saker för skärmen är en smal mobil");
-            MoveCameraWhenThin();
-            MoveShopWhenThin();
-            MoveHealthbarWhenThin();
+            ChangeValuesForThinResolution();
         }
         else if (aspectRatio > aspectRatioNormal && aspectRatio <= aspectRatioWide)
         {
-            //flytta grejer för att de är en skärm som inte är lika fet som en ipad men ändå väldigt bred.
             Debug.Log("Nu händer de grejer för skärmen är bred men inte lika bred som ipad");
-            MoveUpperIconsWide();
-            ScaleMoveShopWhenWide();
-            ScaleMoveHealthbarWhenWide();
+            ChangeValuesForWideResolution();
         }
         else
         {
-            Debug.Log("Nu händer inget för skärmen är inte smal eller bred");
-            MoveUpperIconsNormalScreen();
+            Debug.Log("Nu ändras det till normal värden för skärmen är varken smal eller bred");
+            ChangeValuesForNormalResolution();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 currentScreenSize = new Vector2(Screen.width, Screen.height);
-        if (!currentScreenSize.Equals(lastScreenSize))
-        {
-            OnResolutionChanged();
-            lastScreenSize = currentScreenSize;
-        }
-        
-    }
-    void OnResolutionChanged()
-    {
-        // Din kod för att hantera upplösningsändringar
-        //
+        /* Vector2 currentScreenSize = new Vector2(Screen.width, Screen.height);
+         if (!currentScreenSize.Equals(lastScreenSize))
+         {
+             lastScreenSize = currentScreenSize;
+             CheckAspectRatio();
+         }*/
         float aspectRatio = (float)Screen.width / Screen.height;
-        //Debug.Log(aspectRatio);
+        Debug.Log(aspectRatio);
+
         if (aspectRatio > aspectRatioWide)
         {
             Debug.Log("Nu händer de grejer för skärmen är Ipad bred.");
-            MoveCameraWhenFat();
-            MoveIconsWhenFat();
-            ScaleMoveShopWhenFat();
-            ScaleMoveHealthbarWhenFat();
-            MoveNextRoundButtonWhenFat();
+            ChangeValuesForFatResolution();
         }
         else if (aspectRatio < aspectRatioThinMobile)
         {
             Debug.Log("Nu händer saker för skärmen är en smal mobil");
-            MoveCameraWhenThin();
-            MoveShopWhenThin();
-            MoveHealthbarWhenThin();
+            ChangeValuesForThinResolution();
         }
         else if (aspectRatio > aspectRatioNormal && aspectRatio <= aspectRatioWide)
         {
-            //flytta grejer för att de är en skärm som inte är lika fet som en ipad men ändå väldigt bred.
             Debug.Log("Nu händer de grejer för skärmen är bred men inte lika bred som ipad");
-            MoveUpperIconsWide();
-            ScaleMoveShopWhenWide();
-            ScaleMoveHealthbarWhenWide();
+            ChangeValuesForWideResolution();
         }
         else
         {
-            Debug.Log("Nu händer inget för skärmen är inte smal eller bred");
-            MoveUpperIconsNormalScreen();
+            Debug.Log("Nu ändras det till normal värden för skärmen är varken smal eller bred");
+            ChangeValuesForNormalResolution();
         }
     }
+    private void CheckAspectRatio()
+    {
+        float aspectRatio = (float)Screen.width / Screen.height;
+        Debug.Log(aspectRatio);
+
+        if (aspectRatio > aspectRatioWide)
+        {
+            Debug.Log("Nu händer de grejer för skärmen är Ipad bred.");
+            ChangeValuesForFatResolution();
+        }
+        else if (aspectRatio < aspectRatioThinMobile)
+        {
+            Debug.Log("Nu händer saker för skärmen är en smal mobil");
+            ChangeValuesForThinResolution();
+        }
+        else if (aspectRatio > aspectRatioNormal && aspectRatio <= aspectRatioWide)
+        {
+            Debug.Log("Nu händer de grejer för skärmen är bred men inte lika bred som ipad");
+            ChangeValuesForWideResolution();
+        }
+        else
+        {
+            Debug.Log("Nu ändras det till normal värden för skärmen är varken smal eller bred");
+            ChangeValuesForNormalResolution();
+        }
+    }
+
+
+
+    //samlings metoder
+    private void ChangeValuesForFatResolution()
+    {
+        MoveCameraWhenFat();
+        MoveIconsWhenFat();
+        ScaleMoveShopWhenFat();
+        ScaleMoveHealthbarWhenFat();
+        MoveNextRoundButtonWhenFat();
+    }
+    private void ChangeValuesForWideResolution()
+    {
+        MoveCameraWhenNotFatOrThin();
+        MoveUpperIconsWide();
+        ScaleMoveShopWhenWide();
+        ScaleMoveHealthbarWhenWide();
+        MoveNextRoundButtonWhenWide();
+    }
+    private void ChangeValuesForNormalResolution()
+    {
+        MoveCameraWhenNotFatOrThin();
+        MoveUpperIconsNormalScreen();
+        ScaleMoveShopWhenNormal();
+        ScaleMoveHealthbarWhenNormal();
+        MoveNextRoundButtonWhenNormal();
+    }
+    private void ChangeValuesForThinResolution()
+    {
+        MoveCameraWhenThin();
+        MoveUpperIconsThinScreen();
+        ScaleMoveShopWhenThin();
+        ScaleMoveHealthbarWhenThin();
+        MoveNextRoundButtonWhenThin();
+    }
+
+
     //metoder för FatScreen
     private void MoveCameraWhenFat()
     {
@@ -137,9 +201,9 @@ public class ScaleUI : MonoBehaviour
     }
     private void MoveIconsWhenFat()
     {
-        diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, diamondIcon.transform.position.y + moveUpperIconsValue, diamondIcon.transform.position.z);
-        menuIcon.transform.position = new Vector3(menuIcon.transform.position.x -20f, menuIcon.transform.position.y + moveUpperIconsValue, menuIcon.transform.position.z);
-        mergeArchive.transform.position = new Vector3(menuIcon.transform.position.x, mergeArchive.transform.position.y - mergeFatScreenMoveValue, mergeArchive.transform.position.z);
+        diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, moveUpperIconsFatValue, diamondIcon.transform.position.z);
+        menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, moveUpperMenuButtonsFatValue, menuIcon.transform.position.z);
+        mergeArchive.transform.position = new Vector3(mergeArchive.transform.position.x, moveUpperMenuButtonsFatValue, mergeArchive.transform.position.z);
     }
     private void ScaleMoveShopWhenFat()
     {
@@ -153,41 +217,16 @@ public class ScaleUI : MonoBehaviour
     }
     private void MoveNextRoundButtonWhenFat()
     {
-        playNextRoundButton.transform.position = new Vector3(playNextRoundButton.transform.position.x, playNextRoundButton.transform.position.y - nextRoundButtonFatMoveValue, playNextRoundButton.transform.position.z);
-    }
-
-
-
-    //metoder för thinScreen
-    private void MoveCameraWhenThin()
-    {
-        m_MainCamera.transform.position = new Vector3(m_MainCamera.transform.position.x, cameraPositionThinMobile, m_MainCamera.transform.position.z);
-    }
-    private void MoveShopWhenThin()
-    {
-        shop.GetComponent<RectTransform>().sizeDelta = new Vector2(shop.GetComponent<RectTransform>().sizeDelta.x, shopHeightThinScreen);
-    }
-    private void MoveHealthbarWhenThin()
-    {
-        healthBar.transform.position = new Vector3(healthBar.transform.position.x, healthBar.transform.position.y + healthBarPositionValueThinScreen, healthBar.transform.position.z);
-       
-    }
-
-    //normal värden
-    private void MoveUpperIconsNormalScreen()
-    {
-        diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, diamondIcon.transform.position.y + upperIconsMoveNormalValue, diamondIcon.transform.position.z);
-        menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, menuIcon.transform.position.y + upperIconsMoveNormalValue, menuIcon.transform.position.z);
-        mergeArchive.transform.position = new Vector3(mergeArchive.transform.position.x, mergeArchive.transform.position.y + upperIconsMoveNormalValue, mergeArchive.transform.position.z);
+        playNextRoundButton.transform.position = new Vector3(playNextRoundButton.transform.position.x, nextRoundButtonFatMoveValue, playNextRoundButton.transform.position.z);
     }
 
     // metoder för wideScreen
 
     private void MoveUpperIconsWide()
     {
-        diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, diamondIcon.transform.position.y + upperIconsMoveWhenWide, diamondIcon.transform.position.z);
-        menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, menuIcon.transform.position.y + upperIconsMoveWhenWide, menuIcon.transform.position.z);
-        mergeArchive.transform.position = new Vector3(mergeArchive.transform.position.x, mergeArchive.transform.position.y + upperIconsMoveWhenWide, mergeArchive.transform.position.z);
+        diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, upperIconsMoveWhenWide, diamondIcon.transform.position.z);
+        menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, moveUpperMenuButtonsWideValue, menuIcon.transform.position.z);
+        mergeArchive.transform.position = new Vector3(mergeArchive.transform.position.x, moveUpperMenuButtonsWideValue, mergeArchive.transform.position.z);
     }
     private void ScaleMoveShopWhenWide()
     {
@@ -200,4 +239,62 @@ public class ScaleUI : MonoBehaviour
         healthBar.transform.localScale = new Vector3(healthBarScaleWideScreen, 0.65f, healthBar.transform.localScale.z);
     }
 
+    private void MoveNextRoundButtonWhenWide()
+    {
+        playNextRoundButton.transform.position = new Vector3(playNextRoundButton.transform.position.x, nextRoundButtonWideMoveValue, playNextRoundButton.transform.position.z);
+    }
+
+
+
+    //normal värden
+    private void MoveCameraWhenNotFatOrThin()
+    {
+        m_MainCamera.transform.position = new Vector3(m_MainCamera.transform.position.x, 0, m_MainCamera.transform.position.z);
+    }
+    private void MoveUpperIconsNormalScreen()
+    {
+        diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, upperIconsMoveNormalValue, diamondIcon.transform.position.z);
+        menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, moveUpperMenuButtonsValue, menuIcon.transform.position.z);
+        mergeArchive.transform.position = new Vector3(mergeArchive.transform.position.x, moveUpperMenuButtonsValue, mergeArchive.transform.position.z);
+    }
+    private void ScaleMoveShopWhenNormal()
+    {
+        shop.GetComponent<RectTransform>().sizeDelta = new Vector2(shop.GetComponent<RectTransform>().sizeDelta.x, shopHeightNormalScreen);
+        shop.transform.localScale = new Vector3(shopScaleNormalScreen, shopScaleNormalScreen, shop.transform.localScale.z);
+    }
+    private void ScaleMoveHealthbarWhenNormal()
+    {
+        healthBar.transform.position = new Vector3(healthBar.transform.position.x, healthBarPositionYNormalScreen, healthBar.transform.position.z);
+        healthBar.transform.localScale = new Vector3(healthBarScaleNormalScreen, 0.65f, healthBar.transform.localScale.z);
+    }
+    private void MoveNextRoundButtonWhenNormal()
+    {
+        playNextRoundButton.transform.position = new Vector3(playNextRoundButton.transform.position.x, nextRoundButtonNormalMoveValue, playNextRoundButton.transform.position.z);
+    }
+
+    //metoder för thinScreen
+    private void MoveCameraWhenThin()
+    {
+        m_MainCamera.transform.position = new Vector3(m_MainCamera.transform.position.x, cameraPositionThinMobile, m_MainCamera.transform.position.z);
+    }
+    private void MoveUpperIconsThinScreen()
+    {
+        diamondIcon.transform.position = new Vector3(diamondIcon.transform.position.x, upperIconsMoveNormalValueThin, diamondIcon.transform.position.z);
+        menuIcon.transform.position = new Vector3(menuIcon.transform.position.x, moveUpperMenuButtonsValueThin, menuIcon.transform.position.z);
+        mergeArchive.transform.position = new Vector3(mergeArchive.transform.position.x, moveUpperMenuButtonsValueThin, mergeArchive.transform.position.z);
+    }
+    private void ScaleMoveShopWhenThin()
+    {
+        shop.GetComponent<RectTransform>().sizeDelta = new Vector2(shop.GetComponent<RectTransform>().sizeDelta.x, shopHeightThinScreen);
+        shop.transform.localScale = new Vector3(shopScaleThinScreen, shopScaleThinScreen, shop.transform.localScale.z);
+    }
+    private void ScaleMoveHealthbarWhenThin()
+    {
+        healthBar.transform.position = new Vector3(healthBar.transform.position.x, healthBarPositionValueThinScreen, healthBar.transform.position.z);
+        healthBar.transform.localScale = new Vector3(healthBarScaleThinScreen, healthBarScaleThinScreen, healthBar.transform.localScale.z);
+    }
+    private void MoveNextRoundButtonWhenThin()
+    {
+        playNextRoundButton.transform.position = new Vector3(playNextRoundButton.transform.position.x, nextRoundButtonThinMoveValue, playNextRoundButton.transform.position.z);
+    }
 }
